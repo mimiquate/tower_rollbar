379fed6959e71a661bc587aff6bb1a3a9b62b913
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-11 14:35:24 -0300

    build(deps): supports elixir/erlang 1.13/23

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule TowerRollbar.MixProject do
       app: :tower_rollbar,
       description: @description,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
