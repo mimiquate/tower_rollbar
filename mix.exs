2f1f2df9c005b14e7e3b43eb0c59d230ef95c0d3
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-20 15:14:24 -0300

    back to main

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule TowerRollbar.MixProject do
   defp deps do
     [
       {:jason, "~> 1.4"},
-      {:tower, github: "mimiquate/tower", branch: "dev"},
+      {:tower, github: "mimiquate/tower"},
       {:plug, "~> 1.16"},
 
       # Only needed for Erlang < 25
