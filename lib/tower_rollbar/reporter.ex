62a404b0c708b70afb452323dff4dcaebe07e6ac
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-08-02 13:46:49 -0300

    refactor: from_event

--- lib/tower_rollbar/reporter.ex
+++ lib/tower_rollbar/reporter.ex
@@ -4,72 +4,15 @@ defmodule TowerRollbar.Reporter do
   alias TowerRollbar.Rollbar
 
   @impl true
-  def report_event(%Tower.Event{
-        kind: :error,
-        reason: exception,
-        stacktrace: stacktrace,
-        metadata: metadata
-      }) do
+  def report_event(%Tower.Event{} = event) do
     if enabled?() do
       Rollbar.Client.post(
         "/item",
-        Rollbar.Item.from_exception(exception, stacktrace, plug_conn: plug_conn(metadata))
+        Rollbar.Item.from_event(event)
       )
     else
       IO.puts("Tower.Rollbar NOT enabled, ignoring...")
     end
-  end
-
-  def report_event(%Tower.Event{
-        kind: :throw,
-        reason: reason,
-        stacktrace: stacktrace,
-        metadata: metadata
-      }) do
-    if enabled?() do
-      Rollbar.Client.post(
-        "/item",
-        Rollbar.Item.from_throw(reason, stacktrace, plug_conn: plug_conn(metadata))
-      )
-    else
-      IO.puts("Tower.Rollbar NOT enabled, ignoring...")
-    end
-  end
-
-  def report_event(%Tower.Event{
-        kind: :exit,
-        reason: reason,
-        stacktrace: stacktrace,
-        metadata: metadata
-      }) do
-    if enabled?() do
-      Rollbar.Client.post(
-        "/item",
-        Rollbar.Item.from_exit(reason, stacktrace, plug_conn: plug_conn(metadata))
-      )
-    else
-      IO.puts("Tower.Rollbar NOT enabled, ignoring...")
-    end
-  end
-
-  @impl true
-  def report_event(%Tower.Event{kind: :message, level: level, reason: message}) do
-    if enabled?() do
-      Rollbar.Client.post(
-        "/item",
-        Rollbar.Item.from_message(message, level: level)
-      )
-    else
-      IO.puts("Tower.Rollbar NOT enabled, ignoring...")
-    end
-  end
-
-  defp plug_conn(%{log_event: %{meta: %{conn: conn}}}) do
-    conn
-  end
-
-  defp plug_conn(_) do
-    nil
   end
 
   defp enabled? do
