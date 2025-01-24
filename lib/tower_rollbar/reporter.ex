b8f47ab626ce4afcb495aea85632fc54b325dc75
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-01-24 09:36:08 -0300

    build(deps): updates tower

--- lib/tower_rollbar/reporter.ex
+++ lib/tower_rollbar/reporter.ex
@@ -4,14 +4,16 @@ defmodule TowerRollbar.Reporter do
   alias TowerRollbar.Rollbar
 
   def report_event(%Tower.Event{} = event) do
-    if Rollbar.Client.enabled?() do
-      {:ok, _} =
-        Rollbar.Client.post(
-          "/item",
-          Rollbar.Item.from_event(event)
-        )
-    else
-      IO.puts("TowerRollbar NOT enabled, ignoring...")
-    end
+    Tower.async(fn ->
+      if Rollbar.Client.enabled?() do
+        {:ok, _} =
+          Rollbar.Client.post(
+            "/item",
+            Rollbar.Item.from_event(event)
+          )
+      else
+        IO.puts("TowerRollbar NOT enabled, ignoring...")
+      end
+    end)
   end
 end
--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule TowerRollbar.MixProject do
   defp deps do
     [
       {:jason, "~> 1.4"},
-      {:tower, "~> 0.7.1"},
+      {:tower, github: "mimiquate/tower", branch: "async"},
       {:plug, "~> 1.14"},
 
       # Only needed for Erlang < 25
