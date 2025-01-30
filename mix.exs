b21d2d6204641e5e5f787bafac60f288232642b3
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-01-30 16:36:09 -0300

    build(deps-dev): disambiguate branch for dependabot

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule TowerRollbar.MixProject do
       {:blend, "~> 0.4.0", only: :dev},
 
       # Test
-      {:bypass, github: "mimiquate/bypass", only: :test},
+      {:bypass, github: "mimiquate/bypass", branch: "master", only: :test},
       {:plug_cowboy, "~> 2.7", only: :test},
       {:bandit, "~> 1.5", only: :test}
     ]
