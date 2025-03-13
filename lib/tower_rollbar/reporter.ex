04bd7dbf940f19ee47466c959518181c748e398a
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-03-13 13:27:29 -0300

    keep it async after tower 0.8 became sync

--- lib/tower_rollbar/reporter.ex
+++ lib/tower_rollbar/reporter.ex
@@ -5,13 +5,18 @@ defmodule TowerRollbar.Reporter do
 
   def report_event(%Tower.Event{} = event) do
     if Rollbar.Client.enabled?() do
-      {:ok, _} =
-        Rollbar.Client.post(
-          "/item",
-          Rollbar.Item.from_event(event)
-        )
+      item = Rollbar.Item.from_event(event)
+
+      async(fn ->
+        {:ok, _} = Rollbar.Client.post("/item", item)
+      end)
     else
       IO.puts("TowerRollbar NOT enabled, ignoring...")
     end
+  end
+
+  defp async(fun) do
+    Tower.TaskSupervisor
+    |> Task.Supervisor.start_child(fun)
   end
 end
