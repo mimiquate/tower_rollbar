794ca1eb0dc8c128fe8beb6854b7f38d568c8908
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-09-18 13:40:06 -0300

    use Tower.Igniter.add_reporter_config

--- lib/mix/tasks/tower_rollbar.install.ex
+++ lib/mix/tasks/tower_rollbar.install.ex
@@ -15,37 +15,6 @@ if Code.ensure_loaded?(Igniter) && Code.ensure_loaded?(Tower.Igniter) do
 
     use Igniter.Mix.Task
 
-    import Tower.Igniter
-
-    @default_runtime_config """
-    import Config
-
-    if config_env() == :prod do
-      config :tower_rollbar,
-        access_token: System.get_env("ROLLBAR_SERVER_ACCESS_TOKEN"),
-        environment: System.get_env("DEPLOYMENT_ENV", to_string(config_env()))
-    end
-    """
-
-    @prod_config_patterns [
-      """
-      if config_env() == :prod do
-        __cursor__()
-      end
-      """,
-      """
-      if :prod == config_env() do
-        __cursor__()
-      end
-      """
-    ]
-
-    @rollbar_config_code """
-    config :tower_rollbar,
-      access_token: System.get_env("ROLLBAR_SERVER_ACCESS_TOKEN"),
-      environment: System.get_env("DEPLOYMENT_ENV", to_string(config_env()))
-    """
-
     @impl Igniter.Mix.Task
     def info(_argv, _composing_task) do
       %Igniter.Mix.Task.Info{
@@ -65,108 +34,11 @@ if Code.ensure_loaded?(Igniter) && Code.ensure_loaded?(Tower.Igniter) do
     @impl Igniter.Mix.Task
     def igniter(igniter) do
       igniter
-      |> add_reporter_to_config(TowerRollbar)
-      |> configure_runtime()
-    end
-
-    defp configure_runtime(igniter) do
-      if runtime_config_exists?(igniter) do
-        igniter
-      else
-        add_runtime_config(igniter)
-      end
-    end
-
-    defp runtime_config_exists?(igniter) do
-      Igniter.Project.Config.configures_key?(
-        igniter,
-        "runtime.exs",
+      |> Tower.Igniter.add_reporter_to_config(TowerRollbar)
+      |> Tower.Igniter.add_reporter_config(
         :tower_rollbar,
-        [:access_token]
-      )
-    end
-
-    defp add_runtime_config(igniter) do
-      Igniter.create_or_update_elixir_file(
-        igniter,
-        "config/runtime.exs",
-        @default_runtime_config,
-        &update_runtime_config/1
-      )
-    end
-
-    defp update_runtime_config(zipper) do
-      if Igniter.Project.Config.configures_key?(zipper, :tower_rollbar, :access_token) do
-        {:ok, zipper}
-      else
-        add_config_to_prod_block(zipper)
-      end
-    end
-
-    defp add_config_to_prod_block(zipper) do
-      zipper
-      |> Igniter.Code.Common.move_to_cursor_match_in_scope(@prod_config_patterns)
-      |> case do
-        {:ok, zipper} ->
-          handle_existing_prod_block(zipper)
-
-        :error ->
-          add_prod_block_with_config(zipper)
-      end
-    end
-
-    defp handle_existing_prod_block(zipper) do
-      if Igniter.Project.Config.configures_key?(zipper, :tower_rollbar, :access_token) do
-        {:ok, zipper}
-      else
-        update_existing_config_or_add_new(zipper)
-      end
-    end
-
-    defp update_existing_config_or_add_new(zipper) do
-      case find_existing_rollbar_config(zipper) do
-        {:ok, _zipper} ->
-          update_existing_rollbar_config(zipper)
-
-        _ ->
-          Igniter.Code.Common.add_code(zipper, @rollbar_config_code)
-      end
-    end
-
-    defp find_existing_rollbar_config(zipper) do
-      Igniter.Code.Function.move_to_function_call_in_current_scope(
-        zipper,
-        :=,
-        2,
-        fn call ->
-          Igniter.Code.Function.argument_equals?(call, 0, :tower_rollbar)
-        end
-      )
-    end
-
-    defp update_existing_rollbar_config(zipper) do
-      zipper
-      |> Igniter.Project.Config.modify_config_code(
-        [:access_token],
-        :tower_rollbar,
-        Sourceror.parse_string!(~s[System.get_env("ROLLBAR_SERVER_ACCESS_TOKEN")])
-      )
-      |> Igniter.Project.Config.modify_config_code(
-        [:environment],
-        :tower_rollbar,
-        Sourceror.parse_string!(~s[System.get_env("DEPLOYMENT_ENV", to_string(config_env()))])
-      )
-      |> then(&{:ok, &1})
-    end
-
-    defp add_prod_block_with_config(zipper) do
-      Igniter.Code.Common.add_code(
-        zipper,
-        """
-        if config_env() == :prod do
-          #{@rollbar_config_code}
-        end
-        """
+        access_token: ~s[System.get_env("ROLLBAR_SERVER_ACCESS_TOKEN")],
+        environment: ~s[System.get_env("DEPLOYMENT_ENV", to_string(config_env()))]
       )
     end
   end
--- mix.exs
+++ mix.exs
@@ -41,7 +41,11 @@ defmodule TowerRollbar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tower, "~> 0.7.1 or ~> 0.8.0"},
+      {
+        :tower, "~> 0.7.1 or ~> 0.8.0",
+        github: "mimiquate/tower",
+        branch: "tower-igniter-add-reporter-config"
+      },
       {:plug, "~> 1.14"},
 
       # Optional
