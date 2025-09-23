0f4ab03d2de52323aeb3f76727b99bd3ebf3e5a9
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-09-23 11:38:07 -0300

    bump tower

--- mix.exs
+++ mix.exs
@@ -41,11 +41,7 @@ defmodule TowerRollbar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {
-        :tower,
-        "~> 0.7.1 or ~> 0.8.0",
-        github: "mimiquate/tower"
-      },
+      {:tower, "~> 0.7.1 or ~> 0.8.0"},
       {:plug, "~> 1.14"},
 
       # Optional
