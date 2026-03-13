3939d3386e59ca43534f7801c8d4daa84f8fdf58
Gonzalo <456459+grzuy@users.noreply.github.com>
2026-03-13 14:53:53 -0300

    feat: better handling of Rollbar response

--- lib/tower_rollbar/reporter.ex
+++ lib/tower_rollbar/reporter.ex
@@ -2,13 +2,35 @@ defmodule TowerRollbar.Reporter do
   @moduledoc false
 
   alias TowerRollbar.Rollbar
+  require Logger
 
   def report_event(%Tower.Event{} = event) do
     if Rollbar.Client.enabled?() do
       item = Rollbar.Item.from_event(event)
 
       async(fn ->
-        {:ok, _} = Rollbar.Client.post("/item", item)
+        Rollbar.Client.post("/item", item)
+        |> case do
+          {:error, _} = response ->
+            Logger.error("Network error")
+            response
+
+          {:ok, {{_, status_code, _}, _, body}} when status_code in 400..599 ->
+            body
+            |> TowerRollbar.json_module().decode!()
+            |> case do
+              %{"err" => 1, "message" => message} ->
+                Logger.error(message)
+
+              _ ->
+                Logger.error("Error")
+            end
+
+            nil
+
+          _ ->
+            nil
+        end
       end)
     else
       IO.puts("TowerRollbar NOT enabled, ignoring...")
